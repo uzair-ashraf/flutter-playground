73763718c504dd4730df9908047f852ae2cd5226
Uzair Ashraf <uzinatorcl@gmail.com>
2020-08-26 12:48:29 -0700

    added app bar and scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 
-void main () {
-  runApp(App());
-}
+void main() => runApp(App());
 
 class App extends StatelessWidget {
+  @override
   Widget build(BuildContext ctx) {
     return MaterialApp(
-      home: Text('Hello world!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Quizzerino'),
+        ),
+        body: Text('This is the body'),
+      ),
     );
   }
 }
